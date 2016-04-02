fa7232b841f143b01cb47afe8347af795b722daa
quanghuykckc <quanghuykckc@gmail.com>
2016-04-02 12:52:21 +0700

    Xóa hết dự án

--- SoccerNetwork/gen/com/demo/slidingmenu_tabhostviewpager/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.demo.slidingmenu_tabhostviewpager;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- SoccerNetwork/src/com/enddev/soccernetwork/utils/AppConstant.java
+++
@@ -1,5 +0,0 @@
-package com.enddev.soccernetwork.utils;
-
-public class AppConstant {
-	public static final String HOST_URL = "http://levietnhut-001-site1.1tempurl.com/SoccerNetworkService/";
-}
